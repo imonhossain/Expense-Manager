2ddb2fa204b685a73f76625eb536d55b0fa6ea65
Imon <imon.enroute@gmail.com>
2019-12-01 00:00:43 +0600

    category add

--- app.cpp
+++ app.cpp
@@ -2,45 +2,47 @@
 #include <ctime>
 #include <iterator>
 #include <map>
+#include <bits/stdc++.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//#include "category.cpp"
 using namespace std;
 
+struct Cateogy{
+    int catId;
+    string catName;
+};
 struct Expense{
-    //int eId;
+    int eId;
     string eName;
+    string eAmount;
+    int eCategoryNo;
     string eCategory;
     time_t eTime;
 };
+
+/* all Global Variable */
 int totalExpense = 0;
-Expense exp;
 string expId;
+int totalCategory = 0;
+string cateogryList[200];
+/* Function defination  */
+void getCategoryList();
+void displayCategoryList();
+
 map<int, Expense> expList;
 
-void addExpense(){
-    totalExpense++;
-    Expense exp;
-    //exp.eId = 1;
-    printf("Input Expense name : ");
-    cin>>exp.eName;
-
-    printf("Input Expense Category : ");
-    cin>>exp.eCategory;
-    cout<<endl;
-    exp.eTime = time(0);
-    expList.insert(pair<int, Expense>(totalExpense, exp));
-}
-void displayExpList(){
-    map<int, Expense>::iterator itr;
-    for (itr = expList.begin(); itr != expList.end(); ++itr) {
-        cout << '\t' << itr->second.eName << '\t' << itr->second.eCategory
-             << '\t' << itr->second.eTime << '\n';
-    }
-}
+void addExpense();
+void displayExpList();
 void updateExp(int expId){
 
 }
 int main()
 {
+    getCategoryList();
+    //displayCategoryList();
+
     int n;
     while(1){
         printf("For save your expense press 1 \n");
@@ -57,10 +59,51 @@ int main()
         else{
 
         }
-
     }
 
     cout << endl;
 
     return 0;
+}
+
+
+
+void addExpense(){
+    totalExpense++;
+    Expense exp;
+    printf("Input Expense name : ");
+    std::getline(std::cin >> std::ws, exp.eName);
+    printf("Input Expense Amount : ");
+    cin>>exp.eAmount;
+    printf("Input your expense category id: \n");
+    displayCategoryList();
+    cin>>exp.eCategoryNo;
+    exp.eCategory = cateogryList[exp.eCategoryNo];
+    cout<<endl;
+    exp.eTime = time(0);
+    expList.insert(pair<int, Expense>(totalExpense, exp));
+}
+
+void displayExpList(){
+    map<int, Expense>::iterator itr;
+    int i =1;
+    for (itr = expList.begin(); itr != expList.end(); ++itr) {
+        cout<<i<< ". "<< itr->second.eName << '\t' << itr->second.eCategory
+             << '\t' << itr->second.eTime << '\n';
+    }
+}
+
+
+void getCategoryList(){
+    ifstream MyReadFile("categoryList.txt");
+    string myText;
+    while (getline (MyReadFile, myText)) {
+       totalCategory++;
+       cateogryList[totalCategory] = myText;
+    }
+}
+void displayCategoryList(){
+    for(int i =1; i<=totalCategory; i++){
+        cout<<i<<"."<<'\t'<<cateogryList[i]<<endl;
+    }
 }
