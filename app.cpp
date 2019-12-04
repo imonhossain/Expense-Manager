07aeb1995a5b895334b481881ce46ea9025b5fe1
Imon <imon.enroute@gmail.com>
2019-12-04 23:22:21 +0600

    show balance amount

--- app.cpp
+++ app.cpp
@@ -8,21 +8,24 @@
 #include<string.h>
 //#include "category.cpp"
 using namespace std;
-
+struct User{
+    string name;
+    double balance;
+} user;
 struct Cateogy{
     int catId;
     string catName;
 };
 struct Expense{
-    int eId;
     string eName;
-    string eAmount;
+    double eAmount;
     int eCategoryNo;
     string eCategory;
     time_t eTime;
 };
 
 /* All Global Variable */
+double totalExpenseAmount = 0;
 int totalExpense = 0;
 string expId;
 int totalCategory = 0;
@@ -34,6 +37,8 @@ void displayCategoryList();
 void addCategory();
 void updateCategory();
 void save();
+void deleteExp();
+void showCurrentBalance();
 
 map<int, Expense> expList;
 
@@ -44,15 +49,22 @@ void updateExp(int expId){
 }
 int main()
 {
+    cout<<"Welcome to Expense app \n\nPlease input your name : ";
+    std::getline(std::cin >> std::ws, user.name);
+    cout<<"Input your wallet Amount : ";
+    cin>>user.balance;
+
     getCategoryList();
     int n;
     while(1){
-        printf("1. For save your expense press 1 \n");
+        printf("1. For add your expense press 1 \n");
         printf("2. For display Expense, press 2\n");
         printf("3. For display Category, press 3\n");
         printf("4. For Add category, press 4\n");
         printf("5. For Update category, press 5\n");
-        printf("6. For Save, press 9\n");
+        printf("6. For Delete expense, press 6\n");
+        printf("7. Show Current Balance , press 7\n");
+        printf("9. For Save, press 9\n");
         scanf("%d", &n);
         if(n==1){
             addExpense();
@@ -64,6 +76,10 @@ int main()
             addCategory();
         }else if(n==5){
             updateCategory();
+        }else if(n==6){
+            deleteExp();
+        }else if(n==7){
+            showCurrentBalance();
         }
         else if(n==9){
             save();
@@ -84,6 +100,7 @@ int main()
 
 
 void addExpense(){
+
     totalExpense++;
     Expense exp;
     printf("Input Expense name : ");
@@ -96,18 +113,43 @@ void addExpense(){
     exp.eCategory = cateogryList[exp.eCategoryNo];
     cout<<endl;
     exp.eTime = time(0);
+
+    if(exp.eAmount>user.balance){
+        printf("fail to add. Expense amount is grater than current balance \n ");
+        return;
+    }
+
     expList.insert(pair<int, Expense>(totalExpense, exp));
+    totalExpenseAmount=+ exp.eAmount;
 }
 
 void displayExpList(){
     map<int, Expense>::iterator itr;
     int i =1;
     for (itr = expList.begin(); itr != expList.end(); ++itr) {
-        cout<<i<< ". "<< itr->second.eName << '\t' << itr->second.eCategory
+        cout<<itr->first<< ". "<< itr->second.eName << '\t' << itr->second.eCategory
              << '\t' << itr->second.eTime << '\n';
     }
 }
+void deleteExp(){
+    int id;
+    printf("Please Input your Expense id: ");
+    cin>>id;
+    std::map<int, Expense>::iterator it = expList.find(id);
+    if (it != expList.end())
+    totalExpenseAmount -= it->second.eAmount;
 
+    expList.erase(id);
+}
+void showCurrentBalance(){
+    cout<<"your Current Balance is " << user.balance - totalExpenseAmount<<endl;
+}
+void addAmountToWallet(){
+    double amount;
+    printf("Please Input your Amount : ");
+    cin>>amount;
+    user.balance += amount;
+}
 char cateogryFileName[30];
 char expenseFileName[30];
 void getCategoryList(){
@@ -127,17 +169,6 @@ void addCategory(){
     std::getline(std::cin >> std::ws, name);
     totalCategory++;
     cateogryList[totalCategory] = name;
-    /*
-    ofstream fout;
-    if(cateogryFileName[0] == '\0'){
-        printf("Input your category file name. make sure this file's name max 30 character and same directory  : ");
-        cin>>cateogryFileName;
-        ifstream MyReadFile(cateogryFileName);
-    }
-    fout.open(cateogryFileName);
-    fout << name << endl;
-    fout.close();
-*/
 }
 void save(){
     ofstream catOut;
@@ -162,12 +193,10 @@ void save(){
     int i =1;
     expOut.open(expenseFileName);
     for (itr = expList.begin(); itr != expList.end(); ++itr) {
-
         expOut << itr->second.eName << endl;
         expOut << itr->second.eAmount << endl;
         expOut << itr->second.eCategory << endl;
         expOut << itr->second.eTime << endl;
-
     }
     expOut.close();
 }
